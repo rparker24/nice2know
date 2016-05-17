a42ad1b4a74da79ddbba79a657e18c83fcb42823
rparker24 <robert@robertmparker.net>
2016-05-16 22:09:01 -0400

    categories and other updates

---
+++ controllers/categories_controller.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var Category = require('../models/category.js');
+
+router.get('/', function(req,res) {
+    res.redirect('/categories')
+});
+
+router.get('/categories', function(req,res) {
+
+    Category.findAll({}).then(function(result){
+        var hbsObject = {category : result}
+            res.render('index', hbsObject);
+    })
+});
+
+router.post('/categories/create', function(req,res) {
+    Category.create({
+        category: req.body.name,
+            topic: req.body.topic }).then(function(result){
+                res.redirect('/categories');
+
+    });
+});
+
+router.put('/categories/update/:id', function(req,res) {
+    Category.update({
+        devoured: req.body.topic
+  },
+  {
+        where: { id : req.params.id }
+    }
+        ).then(function (result) {
+            res.redirect('/categories');
+  },
+        function(rejectedPromiseError){
+            console.log(rejectedPromiseError);
+    });
+});
+
+router.delete('/categories/delete/:id', function(req,res) {
+    Category.destroy({
+        where: { id : req.params.id }
+            }).then(function (result) {
+                res.redirect('/categories');
+    },
+        function(rejectedPromiseError){
+            console.log(rejectedPromiseError);
+    });
+});
+
+module.exports = router;
