e1a1a39aa43b45218f5107191ac121abf7157b88
vporta <vporta7@gmail.com>
2016-05-21 21:56:54 -0400

    working on associations

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,37 +4,39 @@ var bcrypt = require('bcryptjs');
 var models = require('../models');
 var session = require('express-session');
 var Fact = require('../models/Fact.js');
-var texter = require('./sms_message');
-var passwords = require('./passwords');
+var User = require('../models/User.js');
+var texter = require('./sms_message'); //require twilio client object
+var passwords = require('./passwords'); //require twilio passwords
 
 
 
-//render new user form
+//render user sign up page/form
 router.get('/users/new', function(req,res) {
   res.render('users/new');
 });
 
+//render user sign in page/form
 router.get('/users/sign-in', function(req,res) {
   res.render('users/sign_in');
 });
 
+//render user to home page when signing out
 router.get('/users/sign-out', function(req,res) {
   req.session.destroy(function(err) {
      res.redirect('/')
   })
 });
 
-router.get('/', function(req,res) {
-    res.redirect('/facts')
-});
+// router.get('/', function(req,res) {
+//     res.redirect('/facts')
+// });
 
+//render home page as index.handlebars
 router.get('/home', function(req,res) {
   res.render('index');
 });
 
-
-  
-
+//create new user
 router.post('/users/create', function(req,res) {
   models.User.findAll({
     where: {$or: [{email: req.body.email}, {username: req.body.username}]}
@@ -43,9 +45,11 @@ router.post('/users/create', function(req,res) {
       res.send("We already have an account with this username");
     } else {
 
+      //hash brownies man!
       bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(req.body.password, salt, function(err, hash) {
 
+          //i love sequelize :!
           User.create({
             username: req.body.username,
             email: req.body.email,
@@ -73,8 +77,8 @@ router.post('/users/login', function(req, res) {
   }).then(function(user) {
     bcrypt.compare(req.body.password, user.password_hash, function(err, result) {
         if (result == true){
-          //make session
-
+          
+          //make a session, bro
           req.session.logged_in = true;
           req.session.user_id = user.id;
           req.session.user_email = user.email;
@@ -84,46 +88,33 @@ router.post('/users/login', function(req, res) {
           res.redirect('/home');
         }
     });
-  })
+  });
 });
 
-//Select text and send message 
-// router.post('/:user_id/facts/:fact_id', function(req, res) {
-//   var targetNumber,
-//       messageBody;
-//     if(req.session.logged_in) {
+//post category value of category id
+router.post('/categories/fact/:id', function(req, res) {
+  if(req.session.logged_in) {
 
-//       sequelize.query("SELECT * FROM facts WHERE id=2", {model: Fact}).then(
-//         function(facts) {
-//           console.log(facts[0]);
-//         })
-//       // sequelize.query('SELECT * FROM facts LEFT JOIN categories AS cats ON cats.id = facts.category_id LEFT JOIN subscriptions AS subs ON subs.category_id = cats.id WHERE subs.user_id = '+req.session.user_id, {model: Fact}).then(function(facts) {
-//           //code
-//           // console.log(facts);
-//           // client.sendMessage({
+    //some more fact finding joy!
+    models.Fact.findAll({
+      where: {$and: [{user_id: req.session.user_id}, {category_id: req.params.id}]}
+    }).then(function(facts) {
 
-//           //     to: "+" + user.countrycode + user.phone, 
-//           //     from: config.twilioNumber, 
-//           //     body: facts.fact 
+      texter.sendMessage({
 
-//           // }, function(err, responseData) { //this function is executed when a response is received from Twilio
+          to: "+" + req.session.countrycode + req.session.phone, //'+' + countrycode + phone
+          from: passwords.twilioNumber, 
+          body: result[randomNum] 
 
-//           //     if (!err) { 
-//           //         console.log(responseData.from); 
-//           //         console.log(responseData.body); 
-//           //     }
-//           // });
-//         // });
+      });
+    });
+  }
+});
+
       
 
 
-//       // sequelize.query('SELECT * FROM facts LEFT JOIN categories AS cats ON cats.id = facts.category_id LEFT JOIN subscriptions AS subs ON subs.category_id = cats.id LEFT JOIN user_facts AS ufs ON ufs.fact_id = facts.id WHERE subs.user_id = ' +req.session.user_id+ 'AND ufs.user_id =' +req.session.user_id+ 'AND facts.id != ufs.fact_id').then(function(facts) {
-//       //     //code
 
-//       // });
-        
-//     }
-// });
 
 // router.post('/users/sendMessage', function() {
 //   var targetNumber,
