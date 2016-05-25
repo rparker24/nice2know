3b96e0c9fb19e965d4fb75013c3a5e517e87ba7b
jimmylado <ladojames92@gmail.com>
2016-05-25 16:42:12 -0400

    added comment for ease

--- config/passwords.js
+++ config/passwords.js
@@ -13,4 +13,4 @@ module.exports = passwords;
 
 //How to store variables locally below
 // export TWILIO_ACCOUNT_SID=vjkwyv74fufwv9c8h49h
-
+// export TWILIO_AUTH_TOKEN=f28181f9134cfca37b7e8ae47364b5b2
