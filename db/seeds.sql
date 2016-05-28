a71dbf353126ad011023442ebd26d916cdfaba45
rparker24 <robert@robertmparker.net>
2016-05-27 22:44:17 -0400

    add category_id to fact table rows

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO facts (fact, topic) VALUES ('There are 18.6 million vacant homes in America. There are 3.1 million homeless people.', 'society');
-INSERT INTO facts (fact, topic) VALUES ('Before the mid-19th century dentures were commonly made with teeth pulled from the mouths of dead soldiers.', 'history');
-INSERT INTO facts (fact, topic) VALUES ('There is enough DNA in an average person’s body to stretch from the sun to Pluto and back. 17 times', 'science');
-INSERT INTO facts (fact, topic) VALUES ('Olympic gold medals are actually made of silver.', 'sports');
-INSERT INTO facts (fact, topic) VALUES ('Every minute, 100 hours of video are uploaded on YouTube by individual users.', 'tech');
-INSERT INTO facts (fact, topic) VALUES ('The chocolate chip cookie was invented by Ruth Graves Wakefield. Now Toll House Cookies.', 'fun');
+INSERT INTO facts (fact, topic, category_id) VALUES ('There are 18.6 million vacant homes in America. There are 3.1 million homeless people.', 'society', 1);
+INSERT INTO facts (fact, topic, category_id) VALUES ('Before the mid-19th century dentures were commonly made with teeth pulled from the mouths of dead soldiers.', 'history', 2);
+INSERT INTO facts (fact, topic, category_id) VALUES ('There is enough DNA in an average person’s body to stretch from the sun to Pluto and back. 17 times', 'science', 3);
+INSERT INTO facts (fact, topic, category_id) VALUES ('Olympic gold medals are actually made of silver.', 'sports', 4);
+INSERT INTO facts (fact, topic, category_id) VALUES ('Every minute, 100 hours of video are uploaded on YouTube by individual users.', 'tech', 5);
+INSERT INTO facts (fact, topic, category_id) VALUES ('The chocolate chip cookie was invented by Ruth Graves Wakefield. Now Toll House Cookies.', 'fun', 6);
 
 INSERT INTO categories (category_name, description) VALUES ('history', 'historical facts');
 INSERT INTO categories (category_name, description) VALUES ('sports', 'sports facts');
